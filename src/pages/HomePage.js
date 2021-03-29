9ead3529ddf0ae3acf10a047611687edfc900378
Karolina Michalak <karolina0michalak@gmail.com>
2021-03-29 16:21:05 +0200

    Styled homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../scss/HomePage.styles.scss';
+import Directory from '../components/menu-item/Directory';
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory/>
+    </div>
+); 
+
+export default HomePage
